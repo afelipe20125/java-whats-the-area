0600f999d59df88aca8d73ccb81a0902fd4f8e6e
20125 <20125@MININT-HADV8T3.columbushs.com>
2019-10-03 14:27:37 -0400

    whats the area

--- src/whats/the/area/WhatsTheArea.java
+++ src/whats/the/area/WhatsTheArea.java
@@ -3,22 +3,61 @@ package whats.the.area;
 
 /**
  *
- * Name:
- * Project Name:
- * Period:
+ * Name: Alejandro felipe
+ * Project Name: whats the area
+ * Period: 7
  *  
  */
 
 // Import Scanner
-
+import java.util.Scanner;
 public class WhatsTheArea {
 
 //    Create Scanner object HERE so you may use the scanner 
 //    throughout all methods
-    
+   private static Scanner in = new Scanner (System.in);
     public static void main(String[] args) {
         // Happy Coding!        
+       int surfaceArea = 0;
+       surfaceArea += triangle();
+       surfaceArea += triangle();
+       surfaceArea += square();
+       surfaceArea -= square();
+       
+       System.out.println("The total area of michaels truck is "+ surfaceArea);
+       
+       
         
     }
+    public static int triangle()
+    {
+     System.out.println("the area of a triangle is 1/2 * base * height");
+        System.out.println("base");
+        int base = in.nextInt();
+        System.out.println("height");
+        int height = in.nextInt();
+        int area = (int)( 0.5 * height * base );  
+        System.out.println("The area of this shape is: "+ area);
+        return area;
+    } 
     
-}
+    public static int square()
+            {
+         System.out.println("height of square");
+         int height = in.nextInt();
+         System.out.println("base of square");
+         int base = in.nextInt();
+         int area = (int)(base * height);
+         System.out.println("The area of this shape is: "+ area);
+         return area;
+                
+                
+            }
+    
+    
+  }
+
+
+
+
+
